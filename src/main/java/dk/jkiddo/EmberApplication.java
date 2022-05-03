25fb18357cc27e830bbd62664a8f26e821e089fe
Jens Kristian Villadsen <jvi@trifork.com>
2022-05-03 10:00:47 +0200

    Extra stuff

--- src/main/java/dk/jkiddo/EmberApplication.java
+++ src/main/java/dk/jkiddo/EmberApplication.java
@@ -5,7 +5,16 @@ import ca.uhn.fhir.context.FhirVersionEnum;
 import ca.uhn.fhir.rest.client.api.ServerValidationModeEnum;
 import ca.uhn.fhir.util.BundleBuilder;
 import com.google.common.base.Strings;
+
+import java.io.BufferedInputStream;
+import java.io.ByteArrayInputStream;
+import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
+import java.net.URI;
+import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.List;
@@ -14,6 +23,7 @@ import java.util.function.Function;
 import java.util.function.Predicate;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
+import org.apache.commons.io.FileUtils;
 import org.hl7.fhir.instance.model.api.IBaseResource;
 import org.hl7.fhir.utilities.npm.FilesystemPackageCacheManager;
 import org.hl7.fhir.utilities.npm.IPackageCacheManager;
@@ -43,6 +53,8 @@ public class EmberApplication implements ApplicationRunner {
   boolean loadRecursively;
   @Value("${includeSearchBundles:false}")
   boolean includeSearchBundles;
+  @Value("${directory:}")
+  String directory;
 
   public static void main(String[] args) {
     LOG.info("STARTING THE APPLICATION");
@@ -53,6 +65,12 @@ public class EmberApplication implements ApplicationRunner {
   @Override
   public void run(ApplicationArguments args) throws Exception {
 
+    if(!Strings.isNullOrEmpty(directory)){
+      loadFiles(directory);
+      System.exit(1);
+    }
+
+
     if (Strings.isNullOrEmpty(igIdAndVersion)) {
       LOG.error("No igIdAndVersion supplied ... - exiting");
       System.exit(-1);
@@ -61,13 +79,34 @@ public class EmberApplication implements ApplicationRunner {
     packageManager = new FilesystemPackageCacheManager(true, new Random().nextInt());
     NpmPackage npmPackage = null;
 
-    try {
-      npmPackage = packageManager.loadPackage(igIdAndVersion);
-    } catch (NullPointerException npe) {
-      LOG.error("Could not load package: " + igIdAndVersion);
-      LOG.info(npe.getMessage(), npe);
-      System.exit(-2);
+    if(igIdAndVersion.contains("file:/"))
+    {
+      var names= igIdAndVersion.split("#");
+      File file = new File(URI.create(names[0]));
+      FileInputStream stream = new FileInputStream(file);
+      packageManager.addPackageToCache(names[2],names[1], stream, "");
+      igIdAndVersion = names[2] + "#" + names[1];
     }
+    if(igIdAndVersion.startsWith("http"))
+    {
+
+      var names= igIdAndVersion.split("#");
+
+      var file = Files.createTempFile(names[2],".tgz");
+      FileUtils.copyURLToFile(URI.create(names[0]).toURL(), file.toFile());
+      FileInputStream stream = new FileInputStream(file.toFile());
+      packageManager.addPackageToCache(names[2],names[1], stream, "");
+      igIdAndVersion = names[2] + "#" + names[1];
+    }
+
+      try {
+        npmPackage = packageManager.loadPackage(igIdAndVersion);
+      } catch (NullPointerException npe) {
+        LOG.error("Could not load package: " + igIdAndVersion);
+        LOG.info(npe.getMessage(), npe);
+        System.exit(-2);
+      }
+
 
     fhirContext = toContext.apply(npmPackage.fhirVersion());
 
@@ -100,6 +139,46 @@ public class EmberApplication implements ApplicationRunner {
       LOG.info("Response from server was: " + fhirContext.newJsonParser().setPrettyPrint(true)
           .encodeResourceToString(response));
     }
+  }
+
+  private void loadFiles(String directory) {
+
+    var fhirContext = FhirContext.forR5();
+
+    List<IBaseResource> resources = Stream.of(new File(directory).listFiles())
+            .filter(file -> !file.isDirectory())
+            .filter(file -> file.getName().endsWith(".json"))
+            .filter(file -> file.getName().contains("mplementatoin"))
+            .map(f -> {
+              try {
+                return new String(Files.readAllBytes(Path.of(f.getPath())));
+              } catch (IOException e) {
+                throw new RuntimeException(e.getMessage(), e);
+              }
+            })
+            .map(fhirContext.newJsonParser().setSuppressNarratives(true)::parseResource)
+            .collect(
+                    Collectors.toList());
+
+
+    var clientFactory = fhirContext.getRestfulClientFactory();
+    clientFactory.setServerValidationMode(ServerValidationModeEnum.NEVER);
+
+    var bundleBuilder = new BundleBuilder(fhirContext);
+    resources.forEach(bundleBuilder::addTransactionCreateEntry);
+    var bundle = bundleBuilder.getBundle();
+
+    if (Strings.isNullOrEmpty(serverBase)) {
+      LOG.info("Sending transaction bundle to console");
+      LOG.info(fhirContext.newJsonParser().setPrettyPrint(true).encodeResourceToString(bundle));
+    } else {
+      LOG.info("Sending transaction bundle to: " + serverBase);
+      var response = clientFactory.newGenericClient(
+              serverBase).transaction().withBundle(bundle).execute();
+      LOG.info("Response from server was: " + fhirContext.newJsonParser().setPrettyPrint(true)
+              .encodeResourceToString(response));
+    }
+
   }
 
   private @NotNull Collection<IBaseResource> loadExampleResourcesRecursively(
